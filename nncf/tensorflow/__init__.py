4f1207277d7b0c4025402a1b059c4e7731eabdf6
Kim, Dongkwan <dongkwan.kim@intel.com>
2022-07-07 22:41:35 +0900

    TF2.8 supportive

--- nncf/tensorflow/__init__.py
+++ nncf/tensorflow/__init__.py
@@ -25,9 +25,10 @@ if not tensorflow_version.startswith(BKC_TF_VERSION[:-2]):
          bkc=BKC_TF_VERSION,
          curr=tensorflow.__version__
     ))
-elif not ('2.4' <= tensorflow_version[:3] <= '2.5'):
+    
+if not ('2.4' <= tensorflow_version[:3] <= '2.8.0'):
    raise RuntimeError(
-        'NNCF only supports tensorflow >=2.4.0, ==2.5.*, while current tensorflow version is {curr}'.format(
+        'NNCF only supports 2.4.0 <= tensorflow <= 2.8.0, while current tensorflow version is {curr}'.format(
         curr=tensorflow.__version__
    ))
 
